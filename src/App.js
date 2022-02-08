a4ae4feb4571d67c9a46f4a80c37af7c2500ec93
Nicolas Lévêque <nicoleveque@outlook.com>
2022-02-08 15:16:47 +0100

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,76 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import Header from './Components/Header'
-import Tasks from './Components/Tasks'
-import AddTask from './Components/AddTask'
-
+import Header from './Components/Header';
+import Tasks from './Components/Tasks';
+import AddTask from './Components/AddTask';
 
 function App() {
-  const [ tasks, setTasks ] = useState([])
-  const [ showAddTask, setShowAddTask ] = useState(false)
+	const [tasks, setTasks] = useState([]);
+	const [showAddTask, setShowAddTask] = useState(false);
 
-  const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
-    const task = await res.json()
-    return task
-  }
-  const deleteTask = async (id) => {
-    
-      const res = await fetch(`http://localhost:5000/tasks/${id}` ,{
-        method: 'DELETE'
-      })
-      res.status ===200 ? setTasks(tasks.filter((task) => task.id !== id)) : alert("Error deleting task")
-    
-  }
-  const addTask = async (task) => {
-    console.log('app', task)
-    const res = await fetch('http://localhost:5000/tasks', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(task)
-    })
-    const data = await res.json()
-    setTasks([ ...tasks, data])
-    setShowAddTask(false)
-  }
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTask(id)
+	const fetchTask = async (id) => {
+		const res = await fetch(`${process.env.REACT_APP_BACKEND}/tasks/${id}`);
+		const task = await res.json();
+		return task;
+	};
+	const deleteTask = async (id) => {
+		const res = await fetch(`${process.env.REACT_APP_BACKEND}/tasks/${id}`, {
+			method: 'DELETE',
+		});
+		res.status === 200
+			? setTasks(tasks.filter((task) => task.id !== id))
+			: alert('Error deleting task');
+	};
+	const addTask = async (task) => {
+		console.log('app', task);
+		const res = await fetch(`${process.env.REACT_APP_BACKEND}/tasks`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(task),
+		});
+		const data = await res.json();
+		setTasks([...tasks, data]);
+		setShowAddTask(false);
+	};
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id);
 
-    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+		const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(updatedTask)  
-    })
-    const data = await res.json()
-    setTasks(
-      tasks.map((task) => 
-        task.id === id ? { ...task, reminder: data.reminder } : task 
-    ))
-  }
+		const res = await fetch(`${process.env.REACT_APP_BACKEND}/tasks/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(updatedTask),
+		});
+		const data = await res.json();
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: data.reminder } : task
+			)
+		);
+	};
 
-  useEffect(()=> {
-    fetch('http://localhost:5000/tasks')
-    .then(response => response.json())
-    .then((json) => {
-    setTasks(json)
-    })
-  }, [])
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_BACKEND}/tasks`)
+			.then((response) => response.json())
+			.then((json) => {
+				setTasks(json);
+			});
+	}, []);
 
-  
-  return (
-    
-    <main className="w-5/6 mx-auto my-6">
-      <Header toggleAddTask={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask}/>
-      {showAddTask ? (<AddTask addTask={addTask}/>): ''}
-      <Tasks data={tasks} onToggle={toggleReminder} onDelete={deleteTask}/>
-      
-    </main>
-  );
+	return (
+		<main className="w-1/2 mx-auto my-6">
+			<Header
+				toggleAddTask={() => setShowAddTask(!showAddTask)}
+				showAddTask={showAddTask}
+			/>
+			{showAddTask ? <AddTask addTask={addTask} /> : ''}
+			<Tasks data={tasks} onToggle={toggleReminder} onDelete={deleteTask} />
+		</main>
+	);
 }
 
 export default App;
